63c766d9a0e75b1486124fb2de0e6c5e0e7bb190
adrian <adrianvez11@gmail.com>
2025-05-04 03:56:55 -0400

    w

--- ToDoApplication/Engines/ListItem.cs
+++ ToDoApplication/Engines/ListItem.cs
@@ -1,6 +1,29 @@
-﻿namespace ToDoApplication.Engines;
+﻿using System.Diagnostics;
+
+namespace ToDoApplication.Engines;
 
 public class ListItem
 {
-    
+    public string _itemName
+    {
+        get { return _itemName; } set { _itemName = value; }
+    }
+
+    public string _itemDescription
+    {
+        get { return _itemDescription;} set { _itemDescription = value; }
+    }
+
+    public bool _itemStatus
+    {
+        get { return _itemStatus; } set { _itemStatus = value; }
+    }
+
+    public ListItem (string itemName, string itemDescription) { _itemName = itemName; _itemDescription = itemDescription; _itemStatus = false; }
+
+    public void RevertStatus()
+    {
+        if (_itemStatus) { _itemStatus = false; }
+        else { _itemStatus = true; }
+    }
 }
